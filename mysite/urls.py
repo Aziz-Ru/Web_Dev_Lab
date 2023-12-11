dbbddec8db0a815030c99f0cba668d5f30173206
silicon <siliconoxide4@gmail.com>
2023-12-12 02:34:00 +0600

    update

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 urlpatterns = [
    
@@ -22,3 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
      path('', views.home),
 ]
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
